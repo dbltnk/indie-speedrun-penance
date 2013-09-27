3f49b251ef85a9e0872c51afc521b483fa05fae6
Sebastian Dorda <sd@bitbarons.com>
2013-09-27 20:00:37 +0200

    still no grid :)

---
+++ Assets/Scripts/GridComponent.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridComponent : MonoBehaviour {
+	public static GridComponent instance;
+
+	public HexGrid grid;
+
+	void Awake () {
+		instance = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
