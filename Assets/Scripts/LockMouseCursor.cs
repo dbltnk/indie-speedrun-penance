9b515aacbc6b0b51cc5ebcd686524e53fff38951
Sebastian Dorda <sd@bitbarons.com>
2013-09-27 23:08:19 +0200

    lock mouse

---
+++ Assets/Scripts/LockMouseCursor.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LockMouseCursor : MonoBehaviour {
+	public bool lockCursorOnStart;
+
+	// Use this for initialization
+	void Start () {
+		Screen.lockCursor = lockCursorOnStart;
+	}
+}
