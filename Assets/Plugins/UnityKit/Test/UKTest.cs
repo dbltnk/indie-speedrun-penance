dbcf74fc5778a1cd296fd65bbf0b106ac441744c
Sebastian Dorda <sd@bitbarons.com>
2013-09-27 19:37:47 +0200

    initial empty project

---
+++ Assets/Plugins/UnityKit/Test/UKTest.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UKTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		TestJobManager ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+
+	#region UKJobManager
+
+	public class TestJob : UKJobManager.Job {
+		public override void OnStart ()
+		{
+			Debug.Log ("job started");
+		}
+
+		public override void OnFinish ()
+		{
+			Debug.Log ("job finished");
+		}
+
+		public override IEnumerator Run ()
+		{
+			int runsLeft = 10;
+			Debug.Log ("job run started");
+			while (!ShouldTerminate && runsLeft >= 0)
+			{
+				--runsLeft;
+				Debug.Log(string.Format("job state {0}", _state));
+				yield return WaitForSeconds (1f);
+			}
+			float t = Time.time;
+			yield return WaitForCondition (() => Time.time - t > 3f);
+			Debug.Log ("job run finished");
+		}
+	}
+
+	void TestJobManager ()
+	{
+		UKJobManager.Run (new TestJob ());
+	}
+
+	#endregion
+}
---
+++ Assets/Plugins/UnityKit/UKTuple.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UKTuple<A,B>
+{
+	public A a;
+	public B b;
+	
+	public UKTuple(A a, B b)
+	{
+		this.a = a;
+		this.b = b;
+	}
+}
+
+public class UKTuple<A,B,C>
+{
+	public A a;
+	public B b;
+	public C c;
+	
+	public UKTuple(A a, B b, C c)
+	{
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+}
+
+public class UKTuple<A,B,C,D>
+{
+	public A a;
+	public B b;
+	public C c;
+	public D d;
+	
+	public UKTuple(A a, B b, C c, D d)
+	{
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		this.d = d;
+	}
+}
