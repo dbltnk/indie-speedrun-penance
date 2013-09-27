d88b8fa693c2c6f0ffcbc1a332be2605b80fc8c5
Sebastian Dorda <sd@bitbarons.com>
2013-09-27 20:38:56 +0200

    wads works

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Soul s = Soul.instance;
+		if (s == null)
+			return;
+
+		transform.position = s.transform.position;
+		var soulPointInForward = s.transform.position + s.transform.InverseTransformDirection (Vector3.forward);
+		transform.LookAt (soulPointInForward);
+	}
+}
---
+++ Assets/Scripts/Soul.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Soul : MonoBehaviour {
+	public static Soul instance;
+
+	private CharacterController _controller;
+
+	public Vector3 gravity;
+	public float movementSpeed;
+
+	void Awake () {
+		instance = this;
+		_controller = GetComponent<CharacterController> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var v = Input.GetAxis ("Vertical");
+		var h = Input.GetAxis ("Horizontal");
+
+		Vector3 movement = transform.TransformDirection (Vector3.forward) * movementSpeed * v;
+		movement += transform.TransformDirection (Vector3.right) * movementSpeed * h;
+
+		_controller.Move((movement + gravity) * Time.deltaTime);
+	}
+}
