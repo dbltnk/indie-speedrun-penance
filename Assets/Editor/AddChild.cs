3f2d2596c35f4be794cf7527821277405c6a3b10
Alexander Zacherl <alexander.zacherl@googlemail.com>
2013-09-28 13:16:41 +0200

    now with broken build

---
+++ Assets/Editor/AddChild.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+
+public class AddChild : ScriptableObject
+{
+	[MenuItem ("GameObject/+Add Child")]
+	static void MenuAddChild()
+	{
+		Transform[] transforms = Selection.GetTransforms(SelectionMode.TopLevel | SelectionMode.OnlyUserModifiable);
+
+		foreach(Transform transform in transforms)
+		{
+			GameObject newChild = new GameObject("_null");
+			newChild.transform.parent = transform;
+			newChild.transform.localPosition = Vector3.zero;
+		}
+
+	}
+}
