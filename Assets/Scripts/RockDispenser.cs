72e260c3996c7668c1209661d080101bbb2c8b0f
Sebastian Dorda <sd@bitbarons.com>
2013-09-28 12:56:24 +0200

    pickup distance, dispenser

---
+++ Assets/Scripts/RockDispenser.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RockDispenser : MonoBehaviour {
+	public static RockDispenser instance;
+
+	public int worldRockLimit = 500;
+	public int gridX;
+	public int gridY;
+
+	public Collider _collider;
+
+	void Awake() {
+		instance = this;
+		_collider = gameObject.FindComponentDeep<Collider> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		PlaceAtRoot ();
+	}
+
+	public void PlaceAtRoot () {
+		var rock = Grid.instance.GetRoot ();
+		transform.position = rock.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
