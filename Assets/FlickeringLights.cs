e65607729da80de54b6778fbc01ee76ae5fbd9b6
Alexander Zacherl <alexander.zacherl@googlemail.com>
2013-09-28 13:01:49 +0200

    added flickering hell light

---
+++ Assets/FlickeringLights.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlickeringLights : MonoBehaviour {
+
+	public float minDuration;
+	public float maxDuration;
+    public Color color0 = Color.red;
+    public Color color1 = Color.yellow;
+	float lastChangeTime = 0f;
+	
+    void Update() {
+		float duration;
+		duration = Random.Range(minDuration, maxDuration);
+		//Debug.Log(duration);
+		if (Time.time - lastChangeTime > duration) {
+        	float t = Mathf.PingPong(Time.time, duration) / duration;
+        	light.color = Color.Lerp(color0, color1, t);
+			lastChangeTime = Time.time;
+		}
+    }
+}
